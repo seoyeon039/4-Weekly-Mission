1473306832bfc2b475c732daf8e57dcdb69d5dd8
seoyeon <ysynop7@namver.com>
2024-03-31 18:06:06 +0900

    Refactor: css background-image로 처리하던 kebab 버튼을 Image 컴포넌트로 처리하도록 수정

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -3,6 +3,7 @@ import { Data } from '@/types/type';
 import formatDate from '@/utils/formatDate';
 import diffTime from '@/utils/diffTime';
 import star from '@/public/images/Icon_star.svg';
+import kebab from '@/public/images/Icon_kebab.svg'
 import noImg from '@/public/images/noImg.svg';
 import styles from '@/styles/LinkCard.module.css';
 import Popover from './Popover';
@@ -27,10 +28,12 @@ function LinkCard({item: {url, id, imageSource, createdAt, description}}: Props)
       <Image className={styles.star} src={star} alt='star' />
       <div
         className={styles.previewImg}
-        style={{ backgroundImage: imageSource ? `url(${imageSource})` : `url(${noImg})`}}
+        style={{ backgroundImage: imageSource ? `url(${imageSource})` : `url(${noImg.src})`}}
       />
       <div className={styles.linkContent}>
-        <button type='button' className={styles.kebab} onClick={handleClick} />
+        <button type='button' className={styles.kebab} onClick={handleClick}>
+          <Image src={kebab} alt='kebab' />
+        </button>
         <Popover isOpen={ isOpen } url={url} />
         <p className={styles.updateTime}>{diffTime(createdAt)}</p>
         <p className={styles.linkDescription}>{description}</p>

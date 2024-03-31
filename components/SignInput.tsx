fba5e2871f1f18e6032ea61e03637117d1e4db21
seoyeon <ysynop7@namver.com>
2024-04-01 04:19:56 +0900

    Feat: SignInput 컴포넌트 구현

---
+++ components/SignInput.tsx
@@ -0,0 +1,75 @@
+import { ChangeEvent, MouseEvent, useState } from 'react';
+import { checkEmailValid } from '@/utils/checkValid';
+import styles from '@/styles/SignInput.module.css';
+import eyeOff from '@/public/images/Icon_eye-off.svg';
+import eyeOn from '@/public/images/Icon_eye-on.svg';
+import Image from 'next/image';
+import errorMessage from '@/constants/error_messages';
+
+interface Props {
+  type: string;
+  placeholder: string;
+}
+
+function SignInput({ type = 'text', placeholder }:Props) {
+  const [isHidden, setIsHidden] = useState(type);
+  const [inputValue, setInputValue] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    isHidden === "password" ? setIsHidden("text") : setIsHidden("password");
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
+  const validEmailInput = () => {
+    if (inputValue === '') {
+      setErrorMsg(errorMessage.EMAIL_REQUIRED);
+      return;
+    } 
+    if (!checkEmailValid(inputValue)) {
+      setErrorMsg(errorMessage.EMAIL_INVALID);
+      return;
+    }
+    setErrorMsg('');
+  }
+
+  const validPasswordInput = () => {
+    if (inputValue === '') {
+      setErrorMsg(errorMessage.PW_REQUIRED);
+      return;
+    }
+    setErrorMsg('');
+  }
+
+  const handleLoad = () => {
+    if (type === "email") return validEmailInput();
+    if (type === "password") return validPasswordInput();
+  }
+
+  return (
+    <div className={styles.content}>
+      <div>
+        <label className={styles.inputName}>{placeholder}</label>
+        <div className={styles.inputContainer}>
+          <input
+            type={isHidden}
+            className={styles.inputBox}
+            onChange={handleChange}
+            onBlur={handleLoad}
+            placeholder={placeholder}/>
+          {type === "password" &&
+            <button className={styles.pwHiddenBtn} type="button" onClick={handleClick}>
+              <Image src={isHidden === "password" ? eyeOff : eyeOn} alt="eye-btn" />
+            </button>
+          }
+          {errorMsg && <div className={styles.errorMessage}>{errorMsg}</div>}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignInput;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import SignInput from "@/components/SignInput"
 import Link from "next/link"
 
 export default function Home() {
@@ -10,6 +11,10 @@ export default function Home() {
       <Link href='/folder'>
         <button>folder 페이지</button>
       </Link>
+      <br/>
+      <p>input 컴포넌트 테스트</p>
+      <SignInput type="email" placeholder="e-mail"/>
+      <SignInput type="password" placeholder="password"/>
     </>
   )
 }

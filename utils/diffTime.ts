9b4b3ffa480f9de8705522f6ef895bd0cb5c29d8
seoyeon <ysynop7@namver.com>
2024-03-31 16:44:41 +0900

    Refactor: 이전 주차에서 사용했던 util 함수 이전 및 date-fns 라이브러리 설치

---
+++ utils/diffTime.ts
@@ -0,0 +1,26 @@
+import { 
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInMonths,
+  differenceInYears
+} from "date-fns";
+
+function diffTime(value: string) {
+  const now = new Date();
+  const date = new Date(value);
+  const min = differenceInMinutes(now, date);
+  const hour = differenceInHours(now, date);
+  const day = differenceInDays(now, date);
+  const month = differenceInMonths(now, date);
+  const year = differenceInYears(now, date);
+
+  if (min < 2) return `1 minutes ago`;
+  if (min < 60) return `${min} minutes ago`;
+  if (hour < 24) return `${hour} hours ago`;
+  if (day < 31) return `${day} days ago`;
+  if (month < 12) return `${month} months ago`;
+  if (month >= 12) return `${year} years ago`;
+}
+
+export default diffTime;
---
+++ utils/formatDate.ts
@@ -0,0 +1,9 @@
+import { getYear, getMonth, getDate } from 'date-fns';
+
+// 날짜가 정수로 떨어지도록 만들어주는 함수.
+function formatDate(value: string) {
+  const date = new Date(value);
+  return `${getYear(date)}. ${getMonth(date) + 1}. ${getDate(date)}`;
+}
+
+export default formatDate;

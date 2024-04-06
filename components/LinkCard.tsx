78217c9345c569e7e028459bd7e03e4e2aae97cb
seoyeon <ysynop7@namver.com>
2024-04-07 03:48:28 +0900

    Refactor: a태그 대신 Link 태그 적용

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -8,6 +8,7 @@ import noImg from '@/public/images/noImg.svg';
 import styles from '@/styles/LinkCard.module.css';
 import Popover from './Popover';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 interface Props {
@@ -23,7 +24,7 @@ function LinkCard({item: {url, id, imageSource, createdAt, description}}: Props)
   }
 
   return (
-    <a href={url} target='_blank' rel='noreferrer'>
+    <Link href={url} target='_blank' rel='noreferrer'>
     <div className={styles.card} key={id}>
       <Image className={styles.star} src={star} alt='star' />
       <div
@@ -40,7 +41,7 @@ function LinkCard({item: {url, id, imageSource, createdAt, description}}: Props)
         <p className={styles.linkCreatedDate}>{formatDate(createdAt)}</p>
       </div>
     </div>
-    </a>
+    </Link>
   )
 }
 

c21d7db451807f61e9bf17a99a24b2d8250390a2
seoyeon <ysynop7@namver.com>
2024-04-06 23:08:00 +0900

    Refactor: svg 컴포넌트 속성 카멜 케이스로 수정

--- components/CloseIconImage.tsx
+++ components/CloseIconImage.tsx
@@ -3,8 +3,8 @@ import React, { SVGProps } from 'react';
 const CloseIconImage = ({fill, stroke}: SVGProps<SVGSVGElement>) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="12" cy="12" r="12" stroke="none" fill={fill}/>
-    <path d="M8 8L16 16" stroke={stroke} stroke-width="2" stroke-linecap="round"/>
-    <path d="M16 8L8 16" stroke={stroke} stroke-width="2" stroke-linecap="round"/>
+    <path d="M8 8L16 16" stroke={stroke} strokeWidth="2" strokeLinecap="round"/>
+    <path d="M16 8L8 16" stroke={stroke} strokeWidth="2" strokeLinecap="round"/>
   </svg>
 )
 

3f36d784d58457c979eb58c01512754e162188db
seoyeon <ysynop7@namver.com>
2024-04-14 03:30:44 +0900

    Refactor: 유저의 프로필이 돋보이도록 css 코드 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,13 @@ interface Props {
 function Header({folderData, folderOwnerData}: Props) {
   return (
     <header className={styles.headerItems}>
-      <Image src={folderOwnerData.image_source} width={64} height={64} alt="userProfile" />
+      <Image
+        src={folderOwnerData.image_source}
+        className={styles.headerItemImg}
+        width={64}
+        height={64}
+        alt="userProfile"
+      />
       <div className={styles.username}>{folderOwnerData.name}</div>
       <div className={styles.folderName}>{folderData.name}</div>
     </header>
